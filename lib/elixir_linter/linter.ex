a3f19e0b244ff92471b4b2f470e8a1ba6f6bbfc4
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2017-01-29 09:42:13 -0500

    use flat map to list all files in dir

--- lib/elixir_linter/linter.ex
+++ lib/elixir_linter/linter.ex
@@ -11,44 +11,43 @@ defmodule ElixirLinter.Linter do
 
   def list_all(filepath) do
     _list_all(filepath)
-    |> _clean_up
   end
 
   defp _list_all(filepath) do 
     cond do
-      String.contains?(filepath, ".git") -> nil
+      String.contains?(filepath, ".git") -> []
       true -> expand(File.ls(filepath), filepath)       
     end
   end
 
   def expand({:ok, files}, path) do
     files
-    |> Enum.map(&_list_all("#{path}/#{&1}"))
+    |> Enum.flat_map(&_list_all("#{path}/#{&1}"))
   end
   def expand({:error, _}, path) do 
-    path
+    [path]
   end
 
-  def delete_all(list, el) do 
-    _delete_all(list, el, []) |> Enum.reverse
-  end
+  # def delete_all(list, el) do 
+  #   _delete_all(list, el, []) |> Enum.reverse
+  # end
 
-  def _delete_all([head | tail], el, new_list) when head === el do
-    _delete_all(tail, el, new_list)
-  end
+  # def _delete_all([head | tail], el, new_list) when head === el do
+  #   _delete_all(tail, el, new_list)
+  # end
 
 
-  def _delete_all([head | tail], el, new_list) do 
-    _delete_all(tail, el, [head | new_list])
-  end
+  # def _delete_all([head | tail], el, new_list) do 
+  #   _delete_all(tail, el, [head | new_list])
+  # end
 
-  def _delete_all([], _, new_list) do 
-    new_list
-  end
+  # def _delete_all([], _, new_list) do 
+  #   new_list
+  # end
 
-  def _clean_up(list) do 
-    list
-    |> List.flatten
-    |> delete_all(nil)
-  end
+  # def _clean_up(list) do 
+  #   list
+  #   |> List.flatten
+  #   |> delete_all(nil)
+  # end
 end
